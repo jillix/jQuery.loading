d0d84f6521d623c070cdd49a18c06ad627f564fa
Cristian Marcel <lcristianiim@yahoo.com>
2015-08-09 00:57:08 +0300

    Loader testing and remove added

--- loader.js
+++ loader.js
@@ -1,6 +1,23 @@
+var div = document.createElement("div");
+var node = document.createTextNode('This will be the loader');
+
 exports.init = function (targetDOM) {
     var self = this;
-    var t = document.createTextNode("This is the loader");
 
-    document.getElementsByTagName(self._config.targetDOM).appendChild(t);
+    div.appendChild(node);
+
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.background = "red";
+    div.style.color = "green";
+    div.style.position = "absolute";
+    div.style.top = "0";
+
+    div.className = self._config.loaderClassName;
+
+    document.getElementsByClassName(self._config.targetDOM)[0].appendChild(div);
+}
+exports.remove = function () {
+    var self = this;
+    document.getElementsByClassName(self._config.loaderClassName)[0].remove();
 }
